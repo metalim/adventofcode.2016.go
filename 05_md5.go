7182894f344c963d92a2566a71dec4935cbc7961
Maxim Litvinov <metalim@gmail.com>
2019-01-15 16:29:44 +0200

    2016.5: optimize: get rid of strconv.Itoa.

--- 05_md5.go
+++ 05_md5.go
@@ -4,7 +4,6 @@ import (
 	"crypto/md5"
 	"fmt"
 	"metalim/advent/2016/lib/source"
-	"strconv"
 
 	. "github.com/logrusorgru/aurora"
 )
@@ -23,25 +22,32 @@ func main() {
 		fmt.Println(Black(id).Bold())
 
 		if par.Part(1) {
+			key := make([]byte, 0, 20)
+			key = append(key, (id + "0")...)
+			first := len(id)
+
 			var pass [8]byte
 			var filled int
-			for i := 0; filled < 8; i++ {
-				data := id + strconv.Itoa(i)
-				hash := md5.Sum([]byte(data))
+			for filled < 8 {
+				hash := md5.Sum(key)
 				if hash[0] == 0 && hash[1] == 0 && hash[2]&0xf0 == 0 {
 					pass[filled] = hex[hash[2]]
 					filled++
 				}
+				key = inc(key, first)
 			}
 			par.Submit(1, string(pass[:]))
 		}
 
 		if par.Part(2) {
+			key := make([]byte, 0, 20)
+			key = append(key, (id + "0")...)
+			first := len(id)
+
 			var pass [8]byte
 			var filled int
-			for i := 0; filled < 8; i++ {
-				data := id + strconv.Itoa(i)
-				hash := md5.Sum([]byte(data))
+			for filled < 8 {
+				hash := md5.Sum(key)
 				if hash[0] == 0 && hash[1] == 0 && hash[2]&0xf0 == 0 {
 					index := hash[2]
 					if index < 8 && pass[index] == 0 {
@@ -49,6 +55,7 @@ func main() {
 						filled++
 					}
 				}
+				key = inc(key, first)
 			}
 			par.Submit(2, string(pass[:]))
 		}
@@ -57,3 +64,22 @@ func main() {
 }
 
 const hex = "0123456789abcdef"
+
+// Just for practice.
+// Manually increment decimal string, up to specified position.
+// Can be replaced by key = id + strconv.Itoa(i), which is slower.
+func inc(key []byte, first int) []byte {
+	for i := len(key) - 1; ; i-- {
+		if key[i] != '9' {
+			key[i]++
+			break
+		}
+		key[i] = '0'
+		if i == first { // all 0, overflow from all 9, need 1 more digit.
+			key = append(key, '0')
+			key[first] = '1'
+			break
+		}
+	}
+	return key
+}
